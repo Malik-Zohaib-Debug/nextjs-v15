a9c90c65420301ce97cf174bba7a315f2d859b6f
Malik-Zohaib-Debug <zohaibzahid112233@gmail.com>
2025-12-23 03:37:11 +0500

    added hero section and shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,25 @@
-const page = () => {
+import SearchForm from "../components/SearchForm";
+
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const query = (await searchParams).query;
+  console.log(query);
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default Home;
