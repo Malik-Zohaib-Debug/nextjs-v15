38ca52101cdfa2bbfd0043f1e1876e26cc77fbf0
Malik-Zohaib-Debug <zohaibzahid112233@gmail.com>
2025-12-23 18:43:59 +0500

    integrated sanity and defined the structures

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "../components/SearchForm";
+import StartupCard from "../components/StartupCard";
 
 const Home = async ({
   searchParams,
@@ -7,6 +8,19 @@ const Home = async ({
 }) => {
   const query = (await searchParams).query;
   console.log(query);
+
+  const posts = [
+    {
+      createdAt: "Yesterday",
+      views: 55,
+      author: { _id: 1 },
+      _id: 1,
+      image:
+        "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3RhcnR1cHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -17,6 +31,16 @@ const Home = async ({
           Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section-container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : `All Startups`}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.map((post: StartupCardType) => (
+            <StartupCard key={posts?.id} post={post} />
+          ))}
+        </ul>
       </section>
     </>
   );
